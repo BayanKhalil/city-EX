ecdbd55c0ec98de58d325214bdbc8168da1e52fc
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-03-31 14:12:14 +0300

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 
 require('dotenv').config()
-const PORT =process.env.PORT ; 
+const PORT =process.env.PORT || 5000; 
 const express = require('express'); 
 const cors = require('cors');
 const superagent = require('superagent');
